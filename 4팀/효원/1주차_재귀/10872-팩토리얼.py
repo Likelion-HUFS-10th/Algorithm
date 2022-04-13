f2c1209adb81258bb289e58c8aedee95a7c6a80c
Hyowon Jeon <68677647+hyowon612@users.noreply.github.com>
2022-04-13 14:22:30 +0900

    Update 10872-팩토리얼.py

--- 4팀/효원/1주차_재귀/10872-팩토리얼.py
+++ 4팀/효원/1주차_재귀/10872-팩토리얼.py
@@ -1,9 +1,9 @@
 num = int(input())
 
 def factorial(a):
-	if a == 0:
-		return 1
-	return a * factorial(a-1)
+    if a == 0:
+        return 1
+    return a * factorial(a-1)
 
 print(factorial(num))
 
