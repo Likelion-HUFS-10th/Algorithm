7a7409da98044333fe399b62dc8927409bb9e317
Hyowon Jeon <68677647+hyowon612@users.noreply.github.com>
2022-04-13 14:23:06 +0900

    Update 10870-피보나치수 5.py

--- 4팀/효원/1주차_재귀/10870-피보나치수 5.py
+++ 4팀/효원/1주차_재귀/10870-피보나치수 5.py
@@ -1,10 +1,10 @@
 num = int(input())
 
 def fibonacci(a):
-	if a == 0:
-		return 0
-	elif a == 1:
-		return 1
-	return fibonacci(a-1) + fibonacci(a-2)
+    if a == 0:
+        return 0
+    elif a == 1:
+        return 1
+    return fibonacci(a-1) + fibonacci(a-2)
 
 print(fibonacci(num))
