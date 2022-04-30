b3afff2bd16abca2393a0405e2ef45e05d7e3f39
leonroars <confynumb@hufs.ac.kr>
2022-04-30 19:21:22 +0900

    마지막 문제 실패

---
+++ 1팀/채호연/LEVEL_5.py
@@ -0,0 +1,37 @@
+#15596 : 정수 N개의 합을 출력하는 함수
+from tkinter import N
+
+
+def solve(a):
+    return sum(a)
+
+#4573 : 셀프넘버 출력 함수
+'''
+첫 번 째 접근 : 대실패 (너무 얄팍하게 접근)
+def selnum():
+    strt = 1
+    diff = 0
+    selfnumber = 0
+    while selfnumber <= 10000:
+        selfnumber = strt + diff
+        print(selfnumber)
+        strt = selfnumber
+
+        if selfnumber < 9:
+            diff = 2
+        else:
+            diff = 11
+'''
+'''두번째 접근 : 1~10000까지의 수 리스트 중 생성자가 있는 수가 아니면 출력'''
+def notselfnum():
+    num = num + sum(map(int, str(num)))
+    return num
+Notselfnum = []
+for p in range(1, 10001):
+    Notselfnum.append(notselfnum(p))
+
+for q in range(1, 10001):
+    if q not in Notselfnum:
+        print(q)
+
+#1065 : 한수 출력 함수
