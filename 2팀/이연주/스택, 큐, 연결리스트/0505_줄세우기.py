bc047b840b325ba4001a460501d5c439b88324c1
Yeonju Lee <430lyj@gmail.com>
2022-05-06 00:07:49 +0900

    Delete 2팀/이연주/스택, 큐, 연결리스트 directory

--- 2팀/이연주/스택, 큐, 연결리스트/0505_줄세우기.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-students = list(map(int, input().split()))
-answer = []
-
-for i in range(n):
-    go = students[i]
-    answer = answer[:i-go] + [i+1] + answer[i-go:]
-
-print(*answer)
