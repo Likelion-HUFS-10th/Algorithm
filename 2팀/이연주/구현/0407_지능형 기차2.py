1334219a7dc511682af16bfe29b22ca8ba3d1752
Yeonju Lee <430lyj@gmail.com>
2022-05-06 00:13:05 +0900

    Delete 2팀/이연주/구현 directory

--- 2팀/이연주/구현/0407_지능형 기차2.py
+++
@@ -1,8 +0,0 @@
-info = [tuple(map(int, input().split())) for _ in range(10)]
-
-answer, maximum = 0, 0
-for outs, ins in info:
-    answer -= outs
-    answer += ins
-    maximum = max(answer, maximum)
-print(maximum)
