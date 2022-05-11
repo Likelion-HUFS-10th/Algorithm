fcf8642f00963a767f7799eeb856a1ece66919c2
Sohyun Eom <88651937+sohy19@users.noreply.github.com>
2022-05-12 02:46:17 +0900

    Create 11501 주식.py

---
+++ 5팀/엄소현/구현/11501 주식.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def get_profit(n, price):
+    profit = 0      # 최대 이익
+    max_price = price[-1]
+    for i in range(n-2, -1, -1):
+        if max_price < price[i]:
+            max_price = price[i]
+        else:
+            profit += max_price - price[i]
+    print(profit)
+
+t = int(input())   # 테스트케이스 개수
+for _ in range(t):
+    n = int(input())   # 날의 수
+    price = list(map(int, input().split()))   # 날 별 주가
+    get_profit(n, price)
