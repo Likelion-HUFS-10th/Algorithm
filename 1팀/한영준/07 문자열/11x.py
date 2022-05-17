280f7568cdceaab09455465310f755f72d4a419c
zizonyoungjun <86956991+zizonyoungjun@users.noreply.github.com>
2022-05-17 16:56:18 +0900

    배열 8 9 11 수정중 ..

---
+++ 1팀/한영준/07 문자열/11x.py
@@ -0,0 +1,17 @@
+list = [1,2,3,4]
+real_list = list
+gop = 1
+gop_list = []
+
+for i in range(len(list)):
+    del list[i]
+    for k in range(len(list)):
+        gop = gop * list[k]
+    gop_list.append(gop)
+    gop = 0
+    list = real_list
+
+#중첩 반복문 -> O(n^2) ㅠㅠ
+    
+       
+print(gop_list)
