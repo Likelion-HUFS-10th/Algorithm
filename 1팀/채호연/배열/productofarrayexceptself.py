bc0effaf52ad35ebba48914bad310df37370a24a
leonroars <confynumb@hufs.ac.kr>
2022-05-18 11:00:16 +0900

    본인제외곱

---
+++ 1팀/채호연/배열/productofarrayexceptself.py
@@ -0,0 +1,13 @@
+#나의 풀이 : 메모리 usage 초과;;
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = []
+        for i in range(len(nums)):
+            nums_popped = nums.pop(i)
+            p = 1
+            for j in nums:
+                p *= j
+                answer.append(p)
+                nums.insert(i, nums_popped)
+        return answer
