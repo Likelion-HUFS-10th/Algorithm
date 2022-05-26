6ba20dd709320a6d0a2b5fd50dc38457ba94b996
leonroars <confynumb@hufs.ac.kr>
2022-05-26 17:00:20 +0900

    두 수의 덧셈

---
+++ 1팀/채호연/연결리스트/addtwonumbers.py
@@ -0,0 +1,42 @@
+'''
+1. 자료형 반환
+class Solution:
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        node, prev = head, None
+
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+    
+        return prev
+    
+    def toList(self, node: ListNode) -> List:
+        list: List = []
+        while node:
+            list.append(node.val)
+            node = node.next
+        
+        return list
+    
+    def toReversedLinkedList(self, result: str) -> ListNode:
+        prev: ListNode = None
+        for r in result:
+            node = ListNode(r)
+            node.next = prev
+            prev = node
+        
+        return node
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        a = self.toList(self.reverseList(l1))
+        b = self.toList(self.reverseList(l2))
+
+        resultStr = int(''.join(str(e) for e in a)) + \
+                    int(''.join(str(e) for e in b))
+        
+        return self.toReversedLinkedList(str(resultStr))
+
+2. 전가산기(Full Adder) 구현
+
+'''
