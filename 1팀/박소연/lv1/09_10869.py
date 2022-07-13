913753e27c578fac94068fa3f6b6f465e9e81581
jellysoo97 <91869170+jellysoo97@users.noreply.github.com>
2022-07-13 15:35:18 +0900

    Delete 1팀/박소연/lv1 directory

--- 1팀/박소연/lv1/09_10869.py
+++
@@ -1,9 +0,0 @@
-# 두 자연수 a, b -> 사칙연산
-
-a, b = map(int, input().split())
-
-print(a + b)
-print(a - b)
-print(a * b)
-print(a // b) # 몫
-print(a % b) # 나머지
--- 1팀/박소연/lv1/12_10430.py
+++
@@ -1,8 +0,0 @@
-# 입력값 받고 사칙연산
-
-a, b, c = map(int, input().split())
-
-print((a + b) % c)
-print(((a % c) + (b % c)) % c)
-print((a * b) % c)
-print(((a % c) * (b % c)) % c)
