e0b51880652b6eddc87f3d4f631d6236d99df07a
jellysoo97 <91869170+jellysoo97@users.noreply.github.com>
2022-07-13 15:32:44 +0900

    lv1

---
+++ 1팀/박소연/Baekjoon/lv1/09_10869.py
@@ -0,0 +1,9 @@
+# 두 자연수 a, b -> 사칙연산
+
+a, b = map(int, input().split())
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b) # 몫
+print(a % b) # 나머지
---
+++ 1팀/박소연/Baekjoon/lv1/12_10430.py
@@ -0,0 +1,8 @@
+# 입력값 받고 사칙연산
+
+a, b, c = map(int, input().split())
+
+print((a + b) % c)
+print(((a % c) + (b % c)) % c)
+print((a * b) % c)
+print(((a % c) * (b % c)) % c)
