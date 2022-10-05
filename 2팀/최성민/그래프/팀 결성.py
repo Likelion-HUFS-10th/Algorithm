519f91963a3c326fbcff309c052726e041505d68
Seongmin Choi <88662637+seongminn@users.noreply.github.com>
2022-10-05 10:51:56 +0900

    Add files via upload

---
+++ 2팀/최성민/그래프/팀 결성.py
@@ -0,0 +1,29 @@
+def find_parent(parent, x):
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
+
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+for i in range(n + 1):
+  parent[i] = i
+
+for _ in range(m):
+  oper, a, b = map(int, input().split())
+  if not oper:
+    union_parent(parent, a, b)
+  elif oper:
+    if find_parent(parent, a) == find_parent(parent, b):
+      print("YES")
+    else:
+      print("NO")
